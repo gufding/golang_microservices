bf1d8f139ad3b5b10900fe6b262de00b7ed567f6
MaximilianHensler <mhensler@email.sc.edu>
2021-07-19 11:33:03 -0600

    Benchmarking tests

--- domain/user_doa_test.go
+++ domain/user_doa_test.go
@@ -3,18 +3,27 @@ package domain
 import (
 	"net/http"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestGetUserNoUserfound(t *testing.T) {
 	user, err := GetUser(0)
 
-	if user != nil {
-		t.Error("We were not expecting a user with ID 0.")
-	}
-	if err == nil {
-		t.Error("We were expecting an error when user ID is 0.")
-	}
-	if err.StatusCode != http.StatusNotFound {
-		t.Error("We were expecting 404 erro when user is not found.")
-	}
+	assert.Nil(t, user, "We were not expecting a user with ID 0.")
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusNotFound, err.StatusCode)
+	assert.EqualValues(t, "User not found.\n", err.Message)
+	assert.EqualValues(t, "not_found\n", err.Code)
+}
+
+func TestGetUserNoError(t *testing.T) {
+	user, err := GetUser(123)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, user)
+	assert.EqualValues(t, 123, user.Id)
+	assert.EqualValues(t, "first", user.FirstName)
+	assert.EqualValues(t, "last", user.LastName)
+	assert.EqualValues(t, "email@gmail.com", user.Email)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/gufding/golang_microservices/mvc
 
 go 1.16
+
+require github.com/stretchr/testify v1.7.0
